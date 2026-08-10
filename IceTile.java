fa65487cbf17b3fca9e35c38961168021a4feff9
oriya200912-a11y <oriya200912@gmail.com>
2026-08-10 23:17:54 +0300

    Update IceTile.java

--- IceTile.java
+++ IceTile.java
@@ -27,20 +27,91 @@ public class IceTile {
     }
 
     public void draw(Graphics g, int x, int y, int size) {
-
+    
+        // קרח רגיל
         if (state == ICE) {
-            g.setColor(Color.WHITE);
+    
+            g.setColor(new Color(190, 235, 250));
+            g.fillRect(x, y, size, size);
+    
+            // מסגרת עדינה
+            g.setColor(new Color(120, 190, 220));
+            g.drawRect(x, y, size, size);
         }
+    
+        // קרח סדוק
         else if (state == CRACKED) {
-            g.setColor(Color.LIGHT_GRAY);
+    
+            g.setColor(new Color(140, 210, 235));
+            g.fillRect(x, y, size, size);
+    
+            g.setColor(new Color(70, 140, 180));
+            g.drawRect(x, y, size, size);
+    
+            // סדקים
+            g.drawLine(
+                    x + size / 2,
+                    y,
+                    x + size / 2 - 8,
+                    y + size / 3
+            );
+    
+            g.drawLine(
+                    x + size / 2 - 8,
+                    y + size / 3,
+                    x + size / 2 + 5,
+                    y + size / 2
+            );
+    
+            g.drawLine(
+                    x + size / 2 + 5,
+                    y + size / 2,
+                    x + size / 2 - 10,
+                    y + size
+            );
+    
+            g.drawLine(
+                    x + size / 2 - 8,
+                    y + size / 3,
+                    x + 5,
+                    y + size / 2
+            );
+    
+            g.drawLine(
+                    x + size / 2 + 5,
+                    y + size / 2,
+                    x + size - 5,
+                    y + size / 3
+            );
         }
-        else {
-            g.setColor(Color.BLUE);
+    
+        // מים
+        else if (state == WATER) {
+    
+            g.setColor(new Color(35, 135, 200));
+            g.fillRect(x, y, size, size);
+    
+            g.setColor(new Color(20, 95, 160));
+            g.drawRect(x, y, size, size);
+    
+            // קווים קטנים שנותנים תחושה של מים
+            g.drawArc(
+                    x + 8,
+                    y + 15,
+                    20,
+                    8,
+                    0,
+                    180
+            );
+    
+            g.drawArc(
+                    x + 30,
+                    y + 35,
+                    20,
+                    8,
+                    0,
+                    180
+            );
         }
-
-        g.fillRect(x, y, size, size);
-
-        g.setColor(Color.BLUE);
-        g.drawRect(x, y, size, size);
     }
 }
