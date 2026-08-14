78c0190dc7d2ae044f8bae63a400e39e45646bc1
oriya200912-a11y <oriya200912@gmail.com>
2026-08-14 14:22:33 +0300

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -54,80 +54,8 @@ public class GamePanel extends JPanel {
         
         setFocusable(true);
 
-        addKeyListener(new KeyAdapter() {
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-
-                if (e.getKeyCode() == KeyEvent.VK_W) {
-                    wPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_S) {
-                    sPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_A) {
-                    aPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_D) {
-                    dPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
-                    upPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    downPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    leftPressed = true;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    rightPressed = true;
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-                if (e.getKeyCode() == KeyEvent.VK_W) {
-                    wPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_S) {
-                    sPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_A) {
-                    aPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_D) {
-                    dPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
-                    upPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    downPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    leftPressed = false;
-                }
-
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    rightPressed = false;
-                }
-            }
-        });
+        inputHandler = new InputHandler();
+        addKeyListener(inputHandler);
 
        while (gameRunning) {
         
