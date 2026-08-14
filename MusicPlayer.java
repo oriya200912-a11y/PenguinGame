77bf6f253219ff5494dcb43c603b000703e7b813
oriya200912-a11y <oriya200912@gmail.com>
2026-08-14 13:32:11 +0300

    Update MusicPlayer.java

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -1,1 +1,32 @@
+import javax.sound.sampled.*;
+import java.io.File;
 
+public class MusicPlayer {
+
+    private Clip clip;
+
+    public void play(String filePath) {
+        try {
+            File file = new File(filePath);
+
+            AudioInputStream audioStream =
+                    AudioSystem.getAudioInputStream(file);
+
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            // ניגון חוזר ברצף
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        if (clip != null) {
+            clip.stop();
+            clip.close();
+        }
+    }
+}
